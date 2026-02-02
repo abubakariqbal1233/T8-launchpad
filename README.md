822f6e7bbfee91d981ca6d9bdcba8b0cd3d22b01
Abubakar651 <abubakariqbal651@gmail.com>
2026-02-02 13:48:51 +0500

    initial Setup

--- README.md
+++
@@ -1,1 +0,0 @@
-# DevOps Learning - Task 1
--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Load configs from System Environment (Default to 'dev' and port 4000)
 ENVIRONMENT = os.getenv("APP_ENV", "dev")
 PORT = int(os.getenv("APP_PORT", 4000))
 
 @app.route("/")
 def home():
+    # Logic to differentiate between environments
     if ENVIRONMENT == "live":
         return "Welcome to Live"
     return "Welcome to Dev"
 
 if __name__ == "__main__":
+    # Host="0.0.0.0" allows the app to be accessible via the EC2 Public IP
     app.run(host="0.0.0.0", port=PORT)
