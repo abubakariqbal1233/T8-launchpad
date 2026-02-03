96030c89c557fb2f2516718859d54261a9ce8130
Abubakar651 <abubakariqbal651@gmail.com>
2026-02-03 14:06:21 +0500

    Remove old workflow file

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Load configs from System Environment (Default to 'dev' and port 4000)
+# Load configs
 ENVIRONMENT = os.getenv("APP_ENV", "dev")
 PORT = int(os.getenv("APP_PORT", 4000))
 
 @app.route("/")
 def home():
-    # Logic to differentiate between environments
     if ENVIRONMENT == "live":
         return "Welcome to Live - VERSION 2.0 (Automated!)"
     return "Welcome to Dev"
 
 if __name__ == "__main__":
-    # Host="0.0.0.0" allows the app to be accessible via the EC2 Public IP
     app.run(host="0.0.0.0", port=PORT)
